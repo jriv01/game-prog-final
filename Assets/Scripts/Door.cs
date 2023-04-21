563828de0879dae5fa624ef6f7aea468acead717
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-04-21 02:28:46 -0400

    Updated world layout and door functionality

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
     public RoomCell[] cellsToActivate;
+    GameObject button;
+
+    void Awake() {
+        button = GameObject.FindGameObjectWithTag("PurchaseButton");
+        button.GetComponent<Button>().onClick.AddListener(() => {print("Clicked");});
+    }
     
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            foreach(RoomCell cell in cellsToActivate) {
-                cell.Activate();
-            }
-            Destroy(gameObject);
+           button.SetActive(true);
+           button.GetComponent<Button>().onClick.AddListener(() => OpenDoor());
         }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            button.GetComponent<Button>().onClick.RemoveAllListeners();
+            button.SetActive(false);
+        }
+    }
+
+    public void OpenDoor() {
+        foreach(RoomCell cell in cellsToActivate) {
+            cell.Activate();
+        }
+        Destroy(gameObject);
     }
 }
