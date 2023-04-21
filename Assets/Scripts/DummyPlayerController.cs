19aa7507c9a353b0e1cc052b333f6366bf412cd5
Theeb12 <ethan.aka.eb@gmail.com>
2023-04-21 04:41:08 -0400

    PlayerMechanicsAdded
    Added an on screen joystick to control player movement. Also added some sprites that are kind of just placeholders for now. They are kind of animated (its just two frames). Character also flips when moving left or right. Will add weapons next.

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DummyPlayerController : MonoBehaviour
 {   
     Rigidbody2D _rigidbody;
     public int speed = 10;
+    public PlayerInput playerControls;
+    public Sprite[] spriteArray;
+    float currTime = 0f;
+    float next = 0.5f;
+    public SpriteRenderer playerSprite;
+    int currSprite = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();    
+        _rigidbody = GetComponent<Rigidbody2D>();
+        playerControls = GetComponent<PlayerInput>();
+        playerSprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -20,8 +30,30 @@ public class DummyPlayerController : MonoBehaviour
     }
 
     void FixedUpdate(){
-        float xSpeed = Input.GetAxis("Horizontal") * speed;
-        float ySpeed = Input.GetAxis("Vertical") * speed;
-        _rigidbody.velocity = new Vector2(xSpeed, ySpeed);
+        Vector2 move = playerControls.actions["Move"].ReadValue<Vector2>();
+        Debug.Log(move);
+        _rigidbody.velocity = move * speed;
+        if(move[0]> 0)
+        {
+            playerSprite.flipX = true;
+        }
+        if(move[0] < 0)
+        {
+            playerSprite.flipX = false;
+        }
+
+        currTime += Time.deltaTime;
+        if (move[0] + move[1] != 0 && currTime >= next)
+        {
+            currTime = 0;
+
+            if (currSprite == 1)
+                currSprite = 2;
+            else
+                currSprite = 1;
+        }
+        else
+            currSprite = 0;
+        playerSprite.sprite = spriteArray[currSprite];
     }
 }
