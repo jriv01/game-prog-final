abbac8515ca6926933ea0749ed58136ae617e842
Theeb12 <ethan.aka.eb@gmail.com>
2023-04-27 22:13:00 -0400

    added the gun and attack axis for the player

--- Assets/Scripts/DummyPlayerController.cs
+++ Assets/Scripts/DummyPlayerController.cs
@@ -13,6 +13,14 @@ public class DummyPlayerController : MonoBehaviour
     float next = 0.5f;
     public SpriteRenderer playerSprite;
     int currSprite = 0;
+    public GameObject gun;
+    Rigidbody2D gunBody;
+    public GameObject bullet;
+    public float nextFire = 0;
+    public float fireRate;
+    public Transform shotSpawn;
+    public GameObject starterShot;
+    public GameObject gunSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +28,42 @@ public class DummyPlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         playerControls = GetComponent<PlayerInput>();
         playerSprite = GetComponent<SpriteRenderer>();
+        gunBody = gun.GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 gunRot = playerControls.actions["Attack"].ReadValue<Vector2>();
+        float gunAngle = Mathf.Atan2(gunRot[0], gunRot[1]) * Mathf.Rad2Deg;
+        /*if (gunAngle > 0)
+        {
+            gunSprite.GetComponent<SpriteRenderer>().flipY = true;
+            
+        }
+        else
+        {
+            gunSprite.GetComponent<SpriteRenderer>().flipY = false;
+        }*/
 
+        if (gunRot[0] != 0 || gunRot[1] != 0)
+        {
+            gunBody.rotation = -gunAngle - 90;
+            if(Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject bulClone = Instantiate(starterShot, shotSpawn.position + new Vector3(0,0,0), shotSpawn.rotation) as GameObject;
+            }
+        }
+
+        else
+            gunBody.rotation = 0;
     }
 
     void FixedUpdate(){
         Vector2 move = playerControls.actions["Move"].ReadValue<Vector2>();
-        Debug.Log(move);
+        //Debug.Log(move);
         _rigidbody.velocity = move * speed;
         if(move[0]> 0)
         {
