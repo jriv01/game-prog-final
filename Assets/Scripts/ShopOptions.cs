6ab536c7c27180c49c9440902725e32a44d119fb
Steven Cantu <91388275+sncantu9999@users.noreply.github.com>
2023-04-28 15:23:36 -0400

    fixed shop

--- Assets/Scripts/ShopOptions.cs
+++ Assets/Scripts/ShopOptions.cs
@@ -8,22 +8,26 @@ public class ShopOptions : MonoBehaviour
     public static int hpCost = 20;
     public static int ammoCost = 10;
     static GameManager _gameManager;
+    GameObject ShopUI;
     // Start is called before the first frame update
     void Start()
     {
         _gameManager = GameObject.FindObjectOfType<GameManager>();
+        ShopUI = GameObject.FindGameObjectWithTag("ShopUI");
     }
 
     public void BuyHP() {
         if(_gameManager.CanAfford(hpCost)) {
             _gameManager.TakeMoney(hpCost);
             print("INCREASE HP");
+            ShopUI.SetActive(false);
         }
     }
 
     public void BuyAmmo() {
         if(_gameManager.CanAfford(ammoCost)) {
             _gameManager.TakeMoney(ammoCost);
+            ShopUI.SetActive(false);
             print("INCREASE AMMO");
         }
     }
