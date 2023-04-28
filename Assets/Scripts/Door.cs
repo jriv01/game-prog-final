73a5981a7d9ecef4112532ffbcb4ca2341ba35e2
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-04-28 02:03:32 -0400

    Fixed doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,35 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class Door : MonoBehaviour
 {
     public RoomCell[] cellsToActivate;
+    public int cost = 20;
+
+    public AudioClip doorLocked;
+    public AudioClip doorUnlock;
+
     GameObject button;
+    TextMeshProUGUI costUI;
+    GameManager gameManager;
+    AudioSource audioSource;
 
     void Awake() {
         button = GameObject.FindGameObjectWithTag("PurchaseButton");
-        button.GetComponent<Button>().onClick.AddListener(() => {print("Clicked");});
+    }
+
+    void Start() {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        costUI = GameObject.FindGameObjectWithTag("CostUI").GetComponent<TextMeshProUGUI>();
+        audioSource = GetComponent<AudioSource>();
     }
     
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-           button.SetActive(true);
-           button.GetComponent<Button>().onClick.AddListener(() => OpenDoor());
+            if(gameManager.CanAfford(cost)) {
+                button.SetActive(true);
+                button.GetComponent<Button>().onClick.AddListener(() => OpenDoor());
+            }
+            else {
+                costUI.enabled = true;
+                costUI.text = "COSTS $" + cost;
+                audioSource.PlayOneShot(doorLocked);
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            button.GetComponent<Button>().onClick.RemoveAllListeners();
-            button.SetActive(false);
+            if(button.activeSelf) {
+                button.GetComponent<Button>().onClick.RemoveAllListeners();
+                button.SetActive(false);
+            } else {
+                costUI.enabled = false;
+            }
+            
         }
     }
 
     public void OpenDoor() {
+        StartCoroutine(UnlockDoor());
+    }
+
+    IEnumerator UnlockDoor() {
+        audioSource.PlayOneShot(doorUnlock);
         foreach(RoomCell cell in cellsToActivate) {
             cell.Activate();
         }
+        gameManager.TakeMoney(cost);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        foreach(Collider2D collide in gameObject.GetComponents<Collider2D>()) {
+            collide.enabled = false;
+        }
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour
 {
     private int coinCount = 0;
-    public TextMeshProUGUI coinUI;
+    
+    TextMeshProUGUI moneyUI;
+    TextMeshProUGUI costUI;
+    GameObject purchaseButton;
 
     private void Awake() {
         // Don't Destroy on Load
@@ -21,16 +25,44 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.FindGameObjectWithTag("PurchaseButton").SetActive(false);
-        coinUI.text = "MONEY: " + coinCount;
+        SceneManager.activeSceneChanged += SceneChange;
+
+        purchaseButton = GameObject.FindGameObjectWithTag("PurchaseButton");
+        costUI = GameObject.FindGameObjectWithTag("CostUI").GetComponent<TextMeshProUGUI>();
+        purchaseButton.SetActive(false);
+        costUI.enabled = false;
+        
+        moneyUI = GameObject.FindGameObjectWithTag("moneyui").GetComponent<TextMeshProUGUI>();
+        UpdateUI();
     }
-    public void resetCoins(){
-        coinCount = 0;
+
+    public void SceneChange(Scene current, Scene next) {
+        moneyUI = GameObject.FindGameObjectWithTag("moneyui").GetComponent<TextMeshProUGUI>();
+        UpdateUI();
     }
+
+    public void UpdateUI() {
+        moneyUI.text = "COINS: " + coinCount;
+    }
+
+    public void ResetCoins(){
+        coinCount = 0;  
+        UpdateUI();
+    }
+
     public void AddMoney(int moneyNum) 
     {
         coinCount += moneyNum;
-        GameObject.FindGameObjectWithTag("moneyui").GetComponent<TextMeshProUGUI>().text = "MONEY: " + coinCount;
+        UpdateUI();
+    }
+    
+    public bool CanAfford(int amount) {
+        return coinCount >= amount;
+    }
+
+    public void TakeMoney(int amount) {
+        coinCount -= amount;
+        UpdateUI();
     }
 
     // Update is called once per frame
